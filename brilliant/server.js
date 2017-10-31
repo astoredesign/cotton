114e6311e6cf276fc0fab3c50480876d69c19edc
astoredesign <alex108@gmail.com>
2017-10-31 09:48:28 +0200

    add article page

--- brilliant/server.js
+++ brilliant/server.js
@@ -1,8 +1,21 @@
 'use strict';
 var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+var fs = require('fs');
 var port = process.env.PORT || 1337;
-
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+var pathName;
+http.createServer(function (request, response) {
+	pathName = url.parse(request.url).pathname;
+	fs.readFile(__dirname + pathName, function (err, data) {
+		if (err) {
+			response.writeHead(404, { 'Content-type': 'text/html' });
+			response.write('Page Was Not Found');
+			response.end();
+		} else {
+			response.writeHead(200, { 'Content-type': 'text/html' });
+			response.write(data);
+			response.end();
+		}
+	});
 }).listen(port);
