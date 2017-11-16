a7d69d27046aad8840ddbe3fcfaeedb978d9f34c
astoredesign <alex108@gmail.com>
2017-11-16 15:27:10 +0200

    Страница артикл Общий леяут Первое подколючение бутстрапа

--- brilliant/server.js
+++ brilliant/server.js
@@ -5,12 +5,18 @@ var querystring = require('querystring');
 var fs = require('fs');
 var port = process.env.PORT || 1337;
 var pathName;
+var ext;
 http.createServer(function (request, response) {
 	pathName = url.parse(request.url).pathname;
+	ext = pathName.substring(pathName.indexOf(".") + 1, pathName.length);
 	fs.readFile(__dirname + pathName, function (err, data) {
 		if (err) {
 			response.writeHead(404, { 'Content-type': 'text/html' });
 			response.write('Page Was Not Found');
+			response.end();
+		} else if (ext == "css") {
+			response.writeHead(200, { 'Content-type': 'text/css' });
+			response.write(data);
 			response.end();
 		} else {
 			response.writeHead(200, { 'Content-type': 'text/html' });
